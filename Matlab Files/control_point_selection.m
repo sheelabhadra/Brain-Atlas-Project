f33d8411e50c091c34802f6451699e3672cf1fff
sheelabhadra <sheelabhadra@gmail.com>
2018-07-08 17:48:15 -0500

    update: Add comments and plots

--- Matlab Files/control_point_selection.m
+++ Matlab Files/control_point_selection.m
@@ -3,4 +3,6 @@
 J = imread('outer_edge.jpg');
 I = imread('base_outer_edge.jpg');
 
+% Lets you select the Control Points on both the images
+% Save the Control Points in the work-space
 cpselect(J, I)
--- Matlab Files/fit_template.m
+++ Matlab Files/fit_template.m
@@ -1,8 +1,15 @@
+% Run this script after generating the Control Points
+% i.e. First run control_point_selection.m to generate
+% movingPoints and fixedPoints
+
+base = imread('base_img_1.jpg');
+template = imread('full_template.jpg');
+
 % tform = fitgeotrans(templatePoints, new_pts, 'lwm', 20); % - Actual program
 
 tform = fitgeotrans(movingPoints, fixedPoints, 'polynomial', 2);
 
-Jregistered = imwarp(J,tform,'OutputView',imref2d(size(I)));
+Jregistered = imwarp(template,tform,'OutputView',imref2d(size(base)));
 
 figure(1)
-imshowpair(Jregistered,I)
+imshowpair(Jregistered,base)
