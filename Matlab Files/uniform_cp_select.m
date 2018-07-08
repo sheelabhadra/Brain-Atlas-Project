3a07b093612ca38ef4a3d4239982501048a83e8b
sheelabhadra <sheelabhadra@gmail.com>
2018-07-08 17:36:43 -0500

    fix: Change nearest point finding function

--- Matlab Files/uniform_cp_select.m
+++ Matlab Files/uniform_cp_select.m
@@ -1,93 +1,110 @@
-% Pick 8 control points uniformly on the template and the base image
-template = imread('outer_edge_template.jpg');
-base = imread('base_outer_edge.jpg');
+function cps = uniform_cp_select(img, nPoints)
+% Currently works for only 8 Control Points
+% No idea about why it doesn't work with more points :(
 
-[i, j] = find(template > 100);
-center = [int32(mean(i)) int32(mean(j))];
+    % Pick 8 control points uniformly on the image
+    [i, j] = find(img > 100);
+    center = [int32(mean(i)) int32(mean(j))];
 
-c1 = 0;
-c2 = 0;
-north = 0;
-south = 0;
-
-for i=1:size(template,1)
+    cps = zeros([4 2]);
+    MaxIt = log(nPoints)/log(2) - 2;
     
-    if template(i,center(2)) > 100
-        if i < 50
-            north = north + i;
-            c1 = c1 + 1;
+    c1 = 0;
+    c2 = 0;
+    north = 0;
+    south = 0;
+
+    for i=1:size(img,1)
+        if img(i,center(2)) > 100
+            if i < 50
+                north = north + i;
+                c1 = c1 + 1;
+            end
+
+            if i > 150
+                south = south + i;
+                c2 = c2 + 1;
+            end  
         end
-        
-        if i > 150
-            south = south + i;
-            c2 = c2 + 1;
-        end  
     end
-end
 
-north = int32(north/c1);
-south = int32(south/c2);
-N = [north center(2)];
-S = [south center(2)];
+    north = int32(north/c1);
+    south = int32(south/c2);
+    N = [north center(2)];
+    S = [south center(2)];
 
-c1 = 0;
-c2 = 0;
-west = 0;
-east = 0;
+    c1 = 0;
+    c2 = 0;
+    west = 0;
+    east = 0;
 
-for i=1:size(template,2)
+    for i=1:size(img,2)
+        if img(center(1),i) > 100
+            if i < 100
+                west = west + i;
+                c1 = c1 + 1;
+            end
+
+            if i > 200
+                east = east + i;
+                c2 = c2 + 1;
+            end  
+        end
+    end
+
+    west = int32(west/c1);
+    east = int32(east/c2);
+    W = [center(1) west];
+    E = [center(1) east];
     
-    if template(center(1),i) > 100
-        if i < 100
-            west = west + i;
-            c1 = c1 + 1;
-        end
-        
-        if i > 200
-            east = east + i;
-            c2 = c2 + 1;
-        end  
-    end
-end
-
-west = int32(west/c1);
-east = int32(east/c2);
-W = [center(1) west];
-E = [center(1) east];
-
-mid_NE = [int32((N(1) + E(1))/2) int32((N(2) + E(2))/2)];
-mid_NW = [int32((N(1) + W(1))/2) int32((N(2) + W(2))/2)];
-mid_SE = [int32((S(1) + E(1))/2) int32((S(2) + E(2))/2)];
-mid_SW = [int32((S(1) + W(1))/2) int32((S(2) + W(2))/2)];
-
-NE = find_nearest_edge(template, mid_NE);
-NW = find_nearest_edge(template, mid_NW);
-SE = find_nearest_edge(template, mid_SE);
-SW = find_nearest_edge(template, mid_SW);
-
-imshow(template);
-hold on;
-
-plot(N(2), N(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(S(2), S(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(E(2), E(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(W(2), W(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(NE(2), NE(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(NW(2), NW(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(SE(2), SE(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-plot(SW(2), SW(1), 'c*', 'LineWidth', 1, 'MarkerSize', 10);
-
-
-function coord = find_nearest_edge(img, TARGET)
+    cps(1,:) = N;
+    cps(2,:) = E;
+    cps(3,:) = S;
+    cps(4,:) = W;
+    
     [row, col] = find(img > 100);
-    distances = 10000.0*ones(size(img,1), size(img,2));
     
-    for i=1:size(row,1)
-        distances(row(i),col(i)) = (double((row(i) - TARGET(1))^2) + double((col(i) - TARGET(2))^2))^0.5;
+    it = 1;
+    while it <= MaxIt
+        temp = zeros(size(cps));
+        disp(cps)
+        for pt=1:size(cps,1)-1
+            mid_pt = [int32((cps(pt,1) + cps(pt+1,1))/2) int32((cps(pt,2) + cps(pt+1,2))/2)];
+            temp(pt,:) = find_perpendicular_point(row, col, mid_pt, cps(pt,:), cps(pt+1,:));
+        end
+        
+        mid_pt = [int32((cps(size(cps,1),1) + cps(1,1))/2) int32((cps(size(cps,1),2) + cps(1,2))/2)];
+        temp(size(cps,1),:) = find_perpendicular_point(row, col, mid_pt, cps(size(cps,1),:), cps(1,:));
+        
+        new = zeros([2^(it+1),2]);
+        
+        % Add temp to the CPs in the correct order
+        for i=1:2^(it+1)
+            new(2*i-1,:) = cps(i,:);
+            new(2*i,:) = temp(i,:);
+        end
+        
+        it = it+1;
+        
+        cps = new;
     end
     
-    min_dist = min(distances(:));
-    
-    [near_row, near_col] = find(distances == min_dist);
-    coord = [near_row, near_col];
+    for pt=1:size(cps,1)
+        cps(pt,:) = fliplr(cps(pt,:));
+    end
+
+end
+
+
+function coord = find_perpendicular_point(row, col, TARGET, pt1, pt2)
+    m1 = double(pt1(1) - pt2(1))/double(pt2(2) - pt1(2));
+    for i=1:size(row,1)
+        % m1*m2 = -1 : perpendicular lines
+        m2 = double(row(i) - TARGET(1))/double(TARGET(2) - col(i));
+        if (m1*m2 < -0.9) && (m1*m2 > -1.1) && (double(row(i) - TARGET(1))^2 + double(TARGET(2) - col(i))^2)^0.5 < 70
+            coord = [row(i), col(i)];
+            break;
+        end
+    end
+%     coord = [0 0];
 end
