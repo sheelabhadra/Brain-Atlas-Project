13877785a475ebe258b45bbfb8ea4c756ef6b160
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-02-22 13:07:41 -0600

    Extracted deep points from matching ROIs

--- Localization/src/deep_features_registration.py
+++ Localization/src/deep_features_registration.py
@@ -14,7 +14,7 @@ from keras.applications.vgg16 import preprocess_input
 from regional_deep_features import cosine_similarity, euclidean_distance
 
 
-def vgg16_features(filename):
+def vgg16_features(img):
     """VGG16 features for an image.
     Args: 
         
@@ -29,14 +29,7 @@ def vgg16_features(filename):
     # get the output of the last maxpooling layer - "block5_pool" or layer[-5]
     layer_name = 'block5_pool'
     get_vgg_features = Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
-
-    # Select the images to extract VGG features from the candidate list.
-    # The candidate list should contain the exact path to the images.
-    img = cv2.imread(filename)
     
-    # Set a fixed input dimension: (320x480)
-    img = cv2.resize(img, (480, 320), interpolation=cv2.INTER_CUBIC)
-
     # Get the VGG16 features
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -78,8 +71,6 @@ def deep_roi(feature1, feature2):
     # Get the feature vectors for the subregions
     roi_num = 0
 
-    print(feature1.shape)
-
     windowed_features_1 = sliding_window(feature1[0], 7)
     windowed_features_2 = sliding_window(feature2[0], 7)
 
@@ -91,19 +82,65 @@ def deep_roi(feature1, feature2):
     return roi_list
 
 
-def deep_points(roi_list):
+def deep_points(img, roi_list):
     """Get points from the ROIs.
 
     """
-    pass
+
+    stride = 32 # 2^5 = 32 (5 max-pooling layers)
+    y_steps = 4
+    x_steps = 9
+
+    # Get the regions from the roi_list in the original image
+    cropped_roi_features, coords = [], []
+
+    for roi_num in roi_list:
+        cropped_img = img[32*(roi_num//x_steps): 224+32*(roi_num//x_steps), 32*(roi_num//y_steps): 224+32*(roi_num//y_steps), :]
+        regions = sliding_window(cropped_img, 32)
+
+        xS = (roi_num//y_steps)*32
+        yS = (roi_num//x_steps)*32
+
+        for i in range(len(regions)):
+            cropped_roi_features.append(vgg16_features(regions[i]))
+            xC = xS + 16 + i%(224 - 32)
+            yC = yS + 16 + i//(224 - 32)
+            coords.append((xC, yC))
+
+    return cropped_roi_features, coords
+
+
+def get_matching_points(que_roi_features, que_coords, ref_roi_features, ref_coords):
+    thresh = 0.5
+    matching_points = []
+
+    i = 0
+    for (qf, rf) in zip(que_roi_features, ref_roi_features):
+        if cosine_similarity(qf.flatten(), rf.flatten()) > thresh:
+            matching_points.append(que_coords[i])
+        i += 1
+
+    return matching_points
 
 
 def main():
-    features_que = vgg16_features('../Data/AP-image-data/train/-0.46/01.jpg')
-    features_ref = vgg16_features('../Data/AP-image-data/train/-0.46/02.jpg')
+    que_img = cv2.imread('../Data/AP-image-data/train/-0.46/01.jpg')
+    que_img = cv2.resize(que_img, (480, 320), interpolation=cv2.INTER_CUBIC)
+    features_que = vgg16_features(que_img)
+
+    ref_img = cv2.imread('../Data/AP-image-data/train/-0.46/02.jpg')
+    ref_img = cv2.resize(ref_img, (480, 320), interpolation=cv2.INTER_CUBIC)
+    features_ref = vgg16_features(ref_img)
 
     roi_list = deep_roi(features_que, features_ref)
-    print(roi_list)
+
+    que_roi_features, que_coords = deep_points(que_img, roi_list)
+
+    ref_roi_features, ref_coords = deep_points(ref_img, roi_list)
+
+    print(matching_points(que_roi_features, que_coords, ref_roi_features, ref_coords))
+
+
 
 if __name__ == "__main__":
     main()
